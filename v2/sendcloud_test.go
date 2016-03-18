e85d6ddaafbe259d62ab5c074165a885cec07b92
yangfeng <smartwalle@gmail.com>
2016-03-18 10:14:52 +0800

    修改名字

--- v2/sendcloud_test.go
+++ v2/sendcloud_test.go
@@ -11,7 +11,7 @@ func Test_SendMailWithTemplate(t *testing.T) {
 	var to = make([]map[string]string, 1)
 	to[0] = map[string]string{"to":"917996695@qq.com", "%url%": "http://www.baidu.com"}
 
-	var ok, err, result = SendMailWithTemplate("mail_verify_cn", "service@smoktech.com", "SMOK", "service@smoktech.com", "SMOK", to)
+	var ok, err, result = SendTemplateMail("mail_verify_cn", "service@smoktech.com", "SMOK", "service@smoktech.com", "SMOK", to)
 
 	fmt.Println(ok, err, result)
 }
